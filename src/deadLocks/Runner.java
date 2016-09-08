ada6584a0164e133bd3d3fb83167c1889f89f230
RAVITEJA <rkadiya@g.clemson.edu>
2016-09-07 21:52:20 -0400

    Some more multithreading.

---
+++ src/deadLocks/Runner.java
@@ -0,0 +1,69 @@
+package deadLocks;
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@SuppressWarnings("InfiniteLoopStatement")
+public class Runner {
+
+    private Account acc1 = new Account();
+    private Account acc2 = new Account();
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+
+    //don't hold several locks at once. If you do, always acquire the locks in the same order
+    //try to get the both locks
+    private void acquireLocks(Lock firstLock, Lock secondLock) throws InterruptedException {
+        while (true) {
+            // Acquire locks
+            boolean gotFirstLock = false;
+            boolean gotSecondLock = false;
+            try {
+                gotFirstLock = firstLock.tryLock();
+                gotSecondLock = secondLock.tryLock();
+            } finally {
+                if (gotFirstLock && gotSecondLock) return;
+                else if (gotFirstLock) firstLock.unlock();
+                else if (gotSecondLock) secondLock.unlock();
+            }
+            // Locks not acquired
+            Thread.sleep(1);
+        }
+    }
+
+    //firstThread runs
+    public void firstThread() throws InterruptedException {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            acquireLocks(lock1, lock2);
+            try {
+                Account.transfer(acc1, acc2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+        }
+    }
+
+    //SecondThread runs
+    public void secondThread() throws InterruptedException {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            acquireLocks(lock2, lock1);
+            try {
+                Account.transfer(acc2, acc1, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+        }
+    }
+
+    //When both threads finish execution, finished runs
+    public void finished() {
+        System.out.println("Account 1 balance: " + acc1.getBalance());
+        System.out.println("Account 2 balance: " + acc2.getBalance());
+        System.out.println("Total balance: " + (acc1.getBalance() + acc2.getBalance()));
+    }
+}
